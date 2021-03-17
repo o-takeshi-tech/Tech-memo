40a26110150ecea289e98101665d9c0d89ad0f99
o-takeshi-tech <takeshi.film@gmail.com>
2021-03-17 12:16:41 +0900

    修正

--- odd_even.rb
+++ odd_even.rb
@@ -1,14 +1,3 @@
-# num = gets.chomp.to_i
-
-# input = []
-# num.times do 
-#     input << gets.chomp
-# end
-
-# input.each do |str|
-#     odd, even = (input.split(//) + [""] * 3).each_slice(2).to_a.then { |h, *t| h.zip(*t).map(&:join) }
-#     puts "#{even.join} #{odd.join}"
-# end
 
 puts "何行入力するか、数字で入力してください"
 number_of_lines = gets.to_i
